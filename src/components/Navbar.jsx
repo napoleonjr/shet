56a84da5724388dc24142fddaf583221667567b3
napoleonjr <146306013+napoleonjr@users.noreply.github.com>
2024-10-28 14:11:59 +0800

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 right-0 m-0 flex items-center justify-between py-6 bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0)) shadow-lg z-10">
       <div className="flex flex-shrink-0 items-center">
-        <h1 className="ml-10 text-3xl font-bold text-purple-600 hover:text-purple-800">N</h1>
+        <h1 className="ml-10 text-3xl font-bold text-purple-600 hover:text-purple-800">AB</h1>
       </div>
       <div className="hidden md:flex items-center justify-center mr-10 gap-6 text-1xl">
         <a href="#home" className="hover:text-purple-600">Home</a>
